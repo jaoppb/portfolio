3217885df812f744d539d6576a1d65b5f0be7b24
jaoppb <joaopedroperes06@gmail.com>
2025-09-04 18:48:21 -0300

    Revert "fix(front): using r2 to fetch models"
    made production ignore glb files instead
    
    This reverts commit b8e15281b7bb04d7cc9edd9b47654bea948e8f4d.

--- front/src/environments/environment.development.ts
+++ front/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     baseURL: document.location.origin,
-    r2BaseURL: 'https://pub-000c8254a26f4c62ae297a8f3595b6db.r2.dev',
+    r2BaseURL: document.location.origin,
 };
